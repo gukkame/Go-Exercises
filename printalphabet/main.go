2f92ebcb0d03871b9dc9c51c097aa71fbea96037
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-07-28 13:40:33 +0300

    first go project

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func main() {
 	var alph string = "abcdefghijklmnopqrstuvwxyz"
 
-	for i := alph[0]; i < alph[25]; i++ {
+	for i := alph[0]; i <= alph[25]; i++ {
 		z01.PrintRune(rune(i))
 	}
 	z01.PrintRune('\n')
