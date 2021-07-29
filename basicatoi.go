8dade99033ccd480cd41f68c936a3d0a6055a8ff
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-07-29 15:31:50 +0300

    GOGOGO9

--- basicatoi.go
+++ basicatoi.go
@@ -1,8 +1,32 @@
 package piscine
 
-import "strconv"
-
 func BasicAtoi(s string) int {
-	intVar, err := strconv.Atoi(s)
-	return intVar
+	const fnAtoi = "Atoi"
+
+	var intSize int
+	sLen := len(s)
+	if intSize == 32 && (0 < sLen && sLen < 10) ||
+		intSize == 64 && (0 < sLen && sLen < 19) {
+		// Fast path for small integers that fit int type.
+		s0 := s
+		if s[0] == '-' || s[0] == '+' {
+			s = s[1:]
+			if len(s) < 1 {
+				return 0
+			}
+		}
+
+		n := 0
+		for _, ch := range []byte(s) {
+			ch -= '0'
+			if ch > 9 {
+				return 0
+			}
+			n = n*10 + int(ch)
+		}
+		if s0[0] == '-' {
+			n = -n
+		}
+		return n
+	}
 }
