b652401805b51e90faafec8098fc5777a9fb6e2e
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-07-29 13:49:42 +0300

    GOGOGO5

--- printstr.go
+++ printstr.go
@@ -3,7 +3,7 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintStr(s string) {
-	for letters := range s {
-		z01.PrintRune(rune(letters))
+	for _, ch := range s {
+		z01.PrintRune(rune(ch))
 	}
 }
