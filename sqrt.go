b12fb55e34992404bef5b80cb8a8f48f6e132269
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-02 15:37:17 +0300

    sqrt

---
+++ sqrt.go
@@ -0,0 +1,23 @@
+package piscine
+
+func Sqrt(nb int) int {
+	upper := nb
+	lower := 0
+	var z int
+
+	for z*z != nb {
+		z = int((upper + lower) / 2)
+		if z*z > nb {
+			upper = z
+		}
+		if z*z < nb {
+			lower = z
+		}
+		if (upper-lower)*(upper-lower) == 1 {
+			z = 0
+			break
+		}
+	}
+
+	return z
+}
