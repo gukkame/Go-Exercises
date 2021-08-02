eaa10baac74a58599c2fc6247c9a181b0795bc73
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-02 10:48:46 +0300

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,13 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb <= 0 {
-		return 0
-	} else {
+	if nb > 0 && nb < 25 {
 		var fact int = 1
 		for i := 1; i <= nb; i++ {
 			fact = fact * i
 		}
 		return fact
+	} else {
+		return 0
 	}
 }
