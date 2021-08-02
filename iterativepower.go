6ef9ff3237a0e0732fd8d916958e76058fe3af48
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-02 12:34:34 +0300

    power

--- iterativepower.go
+++ iterativepower.go
@@ -1,21 +1,18 @@
 package piscine
 
 func IterativePower(nb int, power int) int {
-	if nb > 0 && nb < 25 {
-		var fact int = 1
-		for i := 1; i <= power; i++ {
-
-			fact1 := nb * nb
-			fact = fact1 * nb
-
-		}
-		return fact
-	}
 	if power == 1 {
 		return nb
 	}
-	if nb < 0 || power < 0 {
-		return 0
+	if power == 0 {
+		return 1
+	}
+	if power > 0 {
+		var fact int = 1
+		for i := 1; i <= power; i++ {
+			fact = fact * nb
+		}
+		return fact
 	} else {
 		return 0
 	}
