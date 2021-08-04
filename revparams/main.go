80ea9c2a153e768d0fa2286cfe61178e46545111
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-04 16:22:01 +0300

    okkk

--- revparams/main.go
+++ revparams/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	args := os.Args
-	for _, arg := range args[1+len(args)] {
+	for _, arg := range args[len(args)-2] {
 		s := string([]rune{arg})
 		for _, r := range s {
 			z01.PrintRune(r)
