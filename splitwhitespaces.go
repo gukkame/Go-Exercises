16203468d0625bd6c584a8f5ff65de7dd7e8cd71
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-05 18:12:03 +0300

    whitespace

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -15,6 +15,9 @@ func SplitWhiteSpaces(s string) []string {
 		if letter >= 123 && letter <= 126 {
 			tempString = tempString + string(letter)
 		}
+		if letter >= 48 && letter <= 57 {
+			tempString = tempString + string(letter)
+		}
 		if (letter == 32 && tempString != "") || (letter == 9 && tempString != "") || (letter == 10 && tempString != "") {
 			newString = append(newString, tempString)
 			tempString = ""
