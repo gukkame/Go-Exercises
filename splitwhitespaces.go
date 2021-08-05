3cddfcf7eb79f6a46fb00f67cb4b5deaa8229a13
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-05 18:55:33 +0300

    whitespace

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -6,26 +6,14 @@ func SplitWhiteSpaces(s string) []string {
 	length := len(s) - 1
 
 	for i, letter := range s {
-		if (letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z') {
-			tempString = tempString + string(letter)
-		}
-		if letter >= '!' && letter <= '/' {
-			tempString = tempString + string(letter)
-		}
-		if letter >= 123 && letter <= 126 {
-			tempString = tempString + string(letter)
-		}
-		if letter >= 48 && letter <= 57 {
-			tempString = tempString + string(letter)
-		}
-		if letter >= 91 && letter <= 96 {
+		if letter >= 33 && letter <= 126 {
 			tempString = tempString + string(letter)
 		}
 		if (letter == 32 && tempString != "") || (letter == 9 && tempString != "") || (letter == 10 && tempString != "") {
 			newString = append(newString, tempString)
 			tempString = ""
 		} else if i == length {
-			tempString = tempString + string(letter)
+
 			newString = append(newString, tempString)
 			tempString = ""
 		}
