52ba1399fb43c584ad184289231136d02cd532ba
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-09 13:07:47 +0300

    displayfile

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,12 +8,11 @@ import (
 
 func main() {
 	content, err := ioutil.ReadFile("quest8.txt")
-	if err != nil {
-		fmt.Printf("File name missing")
-	}
-	if len(os.Args) > 2 {
+	if err != nil || len(os.Args) < 2 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
 		fmt.Println("too many arg")
 	} else {
-		fmt.Print(string(content))
+		fmt.Println(string(content))
 	}
 }
