3ca2b330606441ea5254bf559239650e0be366b3
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-09 12:06:55 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
@@ -18,6 +20,22 @@ func main() {
 	points := point{}
 
 	setPoint(&points, 42, 21)
-
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+	p := points.x / 10
+	o := points.x % 10
+	p1 := points.y / 10
+	o1 := points.y % 10
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune(p) + 48)
+	z01.PrintRune(rune(o) + 48)
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.PrintRune(rune(p1) + 48)
+	z01.PrintRune(rune(o1) + 48)
+	z01.PrintRune(10)
 }
