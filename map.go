58ba2c4ab0f4b5f29cd86fd57b74c3f78923d391
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-10 14:12:47 +0300

    map

--- map.go
+++ map.go
@@ -1,19 +1,10 @@
 package piscine
 
 func Map(f func(int) bool, a []int) []bool {
-	for i := range a {
-		f(a[i])
+	r := []bool{}
+	for _, s := range a {
+		r = append(r, f(s))
 	}
-}
 
-func IsPrime(value int) bool {
-	if value <= 1 {
-		return false
-	}
-	for i := 2; i < value; i++ {
-		if value%i == 0 {
-			return false
-		}
-	}
-	return true
+	return r
 }
