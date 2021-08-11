cfc820d86cb293954b380fad6155a5f9abd9f7b4
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-11 12:30:22 +0300

    sorted

--- issorted.go
+++ issorted.go
@@ -2,10 +2,10 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	arr := []bool{}
-	for i := 0; i < len(a); i++ {
-		if f(i, i+1) == 1 {
+	for i := 0; i <= len(a); i++ {
+		if f(i-1, i) == 1 {
 			arr = append(arr, true)
-		} else if f(i, i+1) == -1 {
+		} else if f(i-1, i) == -1 {
 			arr = append(arr, false)
 		} else {
 			arr = append(arr, true)
