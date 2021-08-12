34f03797dcae30feb978783ac8561988574604b8
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-08-12 19:14:07 +0300

    doop

--- doop/main.go
+++ doop/main.go
@@ -4,144 +4,86 @@ import (
 	"os"
 )
 
+func Itoa(n int) string {
+	str := ""
+	var negative string
+	if n < 0 {
+		negative = "-"
+		n *= -1
+	}
+	for i := n; i > 0; i /= 10 {
+		str = string(rune(i%10+48)) + str
+	}
+	return negative + str
+}
+
+func isNumeric(s string) bool {
+	for _, i := range s {
+		if (i >= 48 && i <= 57) || i == 43 || i == 45 {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
-	arguments := os.Args[1:]
-
-	length := 0
-	for i := range arguments {
-		length = i + 1
-	}
-	if length != 3 {
+	var answer int
+	if len(os.Args[1:]) != 3 || (!(isNumeric(os.Args[1])) || !(isNumeric(os.Args[3]))) {
 		return
 	}
-
-	sign := 0
-	if arguments[1] == "+" {
-		sign = 0
-	} else if arguments[1] == "-" {
-		sign = 1
-	} else if arguments[1] == "*" {
-		sign = 2
-	} else if arguments[1] == "/" {
-		sign = 3
-	} else if arguments[1] == "%" {
-		sign = 4
-	} else {
-		os.Stdout.WriteString("0")
+	if len(os.Args[1]) >= 19 || len(os.Args[3]) >= 19 {
 		return
 	}
-
-	for i, s := range arguments[0] {
-		if (s >= '0' && s <= '9') || (i == 0 && s == '-') {
-			continue
-		} else {
-			os.Stdout.WriteString("0\n")
-
-			return
+	n1 := Atoi(os.Args[1])
+	n2 := Atoi(os.Args[3])
+	operator := os.Args[2]
+	if operator == "+" || operator == "-" || operator == "*" || operator == "/" || operator == "%" {
+		if operator == "+" {
+			answer = n1 + n2
+		} else if operator == "-" {
+			answer = n1 - n2
+		} else if operator == "*" {
+			answer = n1 * n2
+		} else if operator == "%" {
+			if n2 == 0 {
+				os.Stdout.WriteString("No modulo by 0")
+				os.Stdout.WriteString("\n")
+				return
+			} else {
+				answer = n1 % n2
+			}
+		} else if operator == "/" {
+			if n2 == 0 {
+				os.Stdout.WriteString("No division by 0")
+				os.Stdout.WriteString("\n")
+				return
+			} else {
+				answer = n1 / n2
+			}
+		}
+		if answer == 0 {
+			os.Stdout.WriteString("0")
+			os.Stdout.WriteString("\n")
+		} else if answer > -9223372036854775808 && 9223372036854775807 > answer {
+			os.Stdout.WriteString(Itoa(answer))
+			os.Stdout.WriteString("\n")
 		}
 	}
-
-	for i, s := range arguments[2] {
-		if (s >= '0' && s <= '9') || (i == 0 && s == '-') {
-			continue
-		} else {
-			os.Stdout.WriteString("0\n")
-
-			return
-		}
-	}
-
-	firstNbr := Atoi(arguments[0])
-	secondNbr := Atoi(arguments[2])
-
-	if secondNbr == 0 && arguments[1] == "/" {
-		s := "No division by 0"
-		os.Stdout.WriteString(s)
-
-		return
-	}
-	if secondNbr == 0 && arguments[1] == "%" {
-		s := "No Modulo by 0"
-		os.Stdout.WriteString(s)
-
-		return
-	}
-
-	result := 0
-	arrayOfFunctions := []func(int, int) int{plus, minus, times, div, mod}
-	result = apply(arrayOfFunctions[sign], firstNbr, secondNbr)
-
-	a := []int{result}
-
-	for _, v := range a { // 45678
-
-		r := rune(v + 48)
-		s := string(r)
-		os.Stdout.WriteString(s)
-	}
-
-	os.Stdout.WriteString("\n")
-}
-
-func plus(a, b int) int {
-	return a + b
-}
-
-func minus(a, b int) int {
-	return a - b
-}
-
-func times(a, b int) int {
-	return a * b
-}
-
-func div(a, b int) int {
-	return a / b
-}
-
-func mod(a, b int) int {
-	return a % b
-}
-
-func apply(f func(int, int) int, a int, b int) int {
-	return f(a, b)
 }
 
 func Atoi(s string) int {
 	runes := []rune(s)
-	LenRune := 0
-	result := 0
-	for i := range runes {
-		LenRune = i + 1
-	}
-	if LenRune == 0 {
-		return 0
-	}
-
-	tens := 1
-	for k := 0; k < LenRune-1; k++ {
-		if runes[k] == '+' || runes[k] == '-' {
-			continue
+	numb := 0
+	for _, s := range runes {
+		for i := '0'; i <= '9'; i++ {
+			if s == i {
+				numb = numb*10 + int(i-'0')
+			}
 		}
-		tens *= 10
 	}
 
-	for i := range runes {
-		if (runes[0] == '-' || runes[0] == '+') && i == 0 {
-			continue
-		}
-		if runes[i] < '0' || runes[i] > '9' {
-			return 0
-		}
-		numb := 0
-		for j := '0'; j < runes[i]; j++ {
-			numb++
-		}
-		result += numb * tens
-		tens /= 10
-	}
 	if runes[0] == '-' {
-		result *= -1
+		numb *= -1
 	}
-	return result
+	return numb
 }
